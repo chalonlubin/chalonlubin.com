9b4bcfcf071d499b481c1cd1e99ecd40ea90d219
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-19 00:33:28 -0800

    Finished about, finished home

--- personal_website/src/routes/About.js
+++ personal_website/src/routes/About.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "../sass/About.scss";
+import chalon from "../assets/chalon.png";
 
 function About() {
-  return "About me";
+  return (
+    <div className="about">
+      <section>
+        <p>
+          Chalon is full-stack software engineer. Whether the task at hand is
+          making a unique and useful frontend web apps while bonding over design
+          decisions with a client, or constructing a secure and robust backend,
+          He is always looking for new ways to improve and expand his skills.
+          <br></br>
+          <br></br>
+          Chalon has also worked as a mental health counselor with a focus on
+          youth, a program director for a mental health non-profit, and as a bar
+          manager for a large brewery.
+          <br></br>
+          <br></br>
+          Chalon is currently interested in full-time work as a software
+          engineer, as well as freelance opportunities in the software industry.
+        </p>
+        <img src={chalon} alt="Chalon" />
+      </section>
+    </div>
+  );
 }
 
 export default About;
--- personal_website/src/routes/Home.js
+++ personal_website/src/routes/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../sass/Home.scss";
+import heroImage from "../assets/hero.png";
 
 function Home() {
-  return "Homepage";
+  const [word, setWord] = React.useState("a freelance web developer.");
+  const words = [
+    "a world traveler.",
+    "an avid Denver Nuggets fan.",
+    "a full stack software engineer.",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setWord(words[Math.floor(Math.random() * words.length)]);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <section className="home">
+        <p>Welcome, I am</p>
+        <h1>
+          <span className="orange">Chalon.</span>
+        </h1>
+        <h2>
+          I am <span id="iAm">{word}</span>
+          <br></br>I'm interested developing projects that solve real world
+          problems, improve lives, inspire creativity or that are just{" "}
+          <span className="pink">plain fun.</span>
+        </h2>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
