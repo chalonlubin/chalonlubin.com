e104feb162035453c1d50c03d98ddb090666e53d
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-15 10:23:17 -0700

    removed DaisyUI

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,23 +4,17 @@ import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
-    <div className="home flex flex-wrap items-center justify-center h-screen w-full">
-      <div
-        className="home-container text-center p-5 mb-44
-      min-w-min"
-      >
-        <p className="home-basic text-lg mb-3"> Welcome to my site 👋</p>
-        <h1 className="home-hero text-4xl mb-3">
+    <div className="home-container flex flex-wrap items-center justify-center h-screen w-full">
+      <div className="home-wrapper text-center p-5 mb-44 min-w-min">
+        <p className="home-welcome text-lg mb-3">Welcome to my site 👋</p>
+        <h1 className="home-hero text-4xl mb-3 tracking-wider">
           Hi! I'm Chalon
-          <br />
-          A Full-Stack
+          <br />A Full-Stack <br />
+          <span className="text-4xl mb-3 text-purple-700 font-bold">
+            Software Engineer
+          </span>
         </h1>
-
-        <h2 className="home-engineer text-4xl mb-3 text-purple-700 font-bold">
-          Software Engineer
-        </h2>
-
-        <h2>
+        <p className="home-welcome text-lg mb-3">
           <Typewriter
             options={{
               strings: [
@@ -35,12 +29,12 @@ function Home() {
               deleteSpeed: 30,
             }}
           />
-        </h2>
-        <div>
-          <button className="bg-black text-white rounded p-3 m-4">
+        </p>
+        <div className="mt-4">
+          <button className="bg-black text-white rounded p-3 mx-2">
             Hire Me
           </button>
-          <button className="bg-purple-700 text-white rounded p-3 m-4">
+          <button className="bg-purple-700 text-white rounded p-3 mx-2">
             Portfolio
           </button>
         </div>
