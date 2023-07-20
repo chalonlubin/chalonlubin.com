d070c1bae4e43e2be45d1bafa0e5aacc0cea3689
Chalon Lubin <chalonlubin1@gmail.com>
2023-07-19 23:02:00 -0700

    Fixed mobile UI issue on homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-4 mx-16">
+    <div className="flex flex-col items-center justify-center pt-2 sm:pt-4 sm:p-4 mx-3 md:mx-10">
       {/* Top section */}
       <div className="flex flex-col md:flex-row items-center justify-center gap-3 w-full">
         <div className=" p-4 w-full md:w-1/2">
           <div className="flex flex-col items-center justify-gap-3 mt-6 font-heading">
-            <p className="text-lg pb-3">Welcome to my site 👋</p>
+            <p className="text-lg pb-3">Welcome to my site ⚡️</p>
             <h1 className="text-4xl text-center">
               I'm Chalon,
-              <br />A Full-Stack <br />
+              <br />
+              Full-Stack <br />
               <span className="text-purple-600 font-semibold">
                 Software Engineer
               </span>
@@ -27,6 +28,8 @@ function Home() {
                     "I enjoy bringing ideas to life, from concept to product.",
                     "I value forward momentum equally as much as learning from failures.",
                     "I love building, both virtually and physically.",
+                    "Does anyone ever read these?",
+                    "If you actually read all of that, you are amazing 😁",
                   ],
                   autoStart: true,
                   loop: true,
@@ -36,7 +39,7 @@ function Home() {
                 }}
               />
             </div>
-            <div className="flex flex-col md:flex-row gap-12">
+            <div className="flex sm:flex-row gap-6 md:gap-10">
               <Link to="contact" className="btn btn-primary">
                 Hire Me
               </Link>
